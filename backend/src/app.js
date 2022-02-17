e6786825080b69db37b2fadbc47a5b50d4dbea3f
nabson <nabsonp@gmail.com>
2022-02-17 00:04:52 -0400

    Permite requisições da porta 3015

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors({
-    origin: 'http://localhost:3021',
+    origin: ['http://localhost:3021', 'http://localhost:3015'],
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
     credentials: true
   }));
